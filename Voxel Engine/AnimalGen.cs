bee2714b8124d41fb475210853e4d365e32e821f
Branden Ten Brink <Tsu@Brandens-MacBook-Pro.local>
2016-02-02 21:11:14 -0500

    scripts

---
+++ Voxel Engine/AnimalGen.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimplexNoise;
+
+public class AnimalGen  {
+
+	int dirtHeight = TerrainGen.dirtHeight;
+	float animalBaseHeight = 1;
+	float animalNoise = 0.04f;
+	float animalNoiseHeight = 3;
+
+	float pigFrequency = 0.2f;
+	int pigDensity = 2;
+
+	GameObject pig;
+
+	public void AnimalGeneration(Chunk chunk) 
+	{
+		for (int x = chunk.pos.x-3; x < chunk.pos.x + Chunk.chunkSize + 3; x++)
+		{
+			for (int z = chunk.pos.z-3; z < chunk.pos.z + Chunk.chunkSize+3; z++)
+			{
+				AnimalPositionGen(chunk, x, z);
+			}
+		}
+	}
+
+	public void AnimalPositionGen(Chunk chunk, int x, int z){
+		int animalHeight = dirtHeight + Mathf.FloorToInt(animalBaseHeight);
+		animalHeight += GetNoise(x, 100 , z, animalNoise, Mathf.FloorToInt(animalNoiseHeight));
+		
+		for (int y = chunk.pos.y-8; y < chunk.pos.y + Chunk.chunkSize; y++)
+		{
+			if (y == animalHeight && GetNoise(x, y, z, pigFrequency, 100) < pigDensity)
+			{
+				CreatePig(x,y+1,z);
+			}
+		}
+	}
+
+	public static int GetNoise(int x, int y, int z, float scale, int max) 
+	{
+		return Mathf.FloorToInt( (Noise.Generate(x*scale,y*scale, z*scale) + 1f) * (max/2f));
+	}
+
+	public void CreatePig(int x, int y, int z){
+		pig = MonoBehaviour.Instantiate (Resources.Load ("Prefabs/Pig",typeof(GameObject)),new Vector3(x,y,z),Quaternion.identity) as GameObject;
+		World.pigs.Add (pig);
+	}
+}
---
+++ Voxel Engine/Modify.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Modify : MonoBehaviour {
+
+	int activeBlock = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Alpha1)) {
+			activeBlock = 1;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			activeBlock = 2;
+		} else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			activeBlock = 3;
+		} 
+
+		if (Input.GetMouseButtonDown (1)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, 100)) {
+				if (hit.distance < 10) {
+					EditTerrain.DestroyBlock (hit, new BlockAir ());
+				}
+			}
+		} else if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, 100)) {
+				if (hit.distance < 5) {
+					switch (activeBlock) {
+					case 1: 
+						EditTerrain.SetBlock (hit, new Block (), true);
+						break;
+					
+					case 2:
+						EditTerrain.SetBlock (hit, new BlockGrass (), true);
+						break;
+					
+					case 3:
+						EditTerrain.SetBlock (hit, new BlockWood (), true);
+						break;
+
+					default :
+						break;
+					}
+
+				}
+			}
+		}
+	}
+
+	void OnApplicationQuit () {
+		var chunksToDelete = new List<WorldPos> ();
+		var world = GameObject.FindObjectOfType<World> ();
+
+		foreach (var chunk in world.chunks) {
+			chunksToDelete.Add (chunk.Key);
+		}
+
+		foreach (var chunk in chunksToDelete) {
+			world.DestroyChunk(chunk.x,chunk.y,chunk.z);
+		}
+	}
+}
